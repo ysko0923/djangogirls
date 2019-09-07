5ecc989ac30079a3aeab14ef4917b7e4c74262ec
yoonseo <ysko0923@gmail.com>
2019-09-07 16:59:33 +0900

    commit
    mywebsite open

---
+++ intro.py
@@ -0,0 +1,14 @@
+def hi(name):
+    print('hello' + name)
+
+girls = ['yoonseo','jenny','jiny']
+for name in girls:
+    hi(name)
+
+for a in range(1,6):
+    print(a)
+
+i=0
+while i<5:
+    print(i)
+    i+=1
